a842b79c31a14b901f607d566b863259c9c6f206
Nishanth-euec <nishanthsubramani555@gmail.com>
2025-03-26 16:47:55 +0530

    Initial Webdriver project

---
+++ src/test/java/testScripts/DemoTest.java
@@ -0,0 +1,25 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+    @Test
+  public void f() {
+    	WebDriver driver=new ChromeDriver();
+    	driver.get("https://the-internet.herokuapp.com/login");
+    	WebElement name=driver.findElement(By.id("username"));
+    	WebElement password=driver.findElement(By.name("password"));
+    	name.sendKeys("tomsmith");
+    	password.sendKeys("SuperSecretPassword!");
+    	WebElement login_btn=driver.findElement(By.tagName("button"));
+    	login_btn.click();
+    	driver.quit();
+    	
+  }
+  
+  
+}
---
+++ src/test/java/testScripts/DemoTest1.java
@@ -0,0 +1,17 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+  @Test
+  public void first() {
+	  WebDriver driver=new ChromeDriver();
+
+	  
+	  
+	  
+	  
+  }
+}
